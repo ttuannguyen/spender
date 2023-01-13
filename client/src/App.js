4ef383fcad08864f5cf698e51a19f32a6e4c28c7
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-01-12 22:31:20 -0500

    install React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Categories from './components/Categories';
 import ExpenseForm from './components/ExpenseForm';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
+
 
 const App = () => {
+
   return (
     <div className="App">
+      <Router >
+        <h1>Capstone Project</h1>
+        <Routes>
+          <Route exact path="/home" element={ <Home /> } />
+        </Routes>
+      </Router>
       <Home />
-      <Navbar />
-      <Categories />
-      <ExpenseForm />
     </div>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h3>Welcome to My Capstone Project</h3>
+        <h3>Welcome to Home Page</h3>
+        <h4>Usage Instructions</h4>
+        <p>Go to Categories to Add an Expense</p>
+        <p>Go to a Category to view a list of your expenses in that catogory</p>
     </div>
   )
 }
