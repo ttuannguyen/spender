fc2a00eba404f961f64e5233815b970654c19ae1
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-01-15 16:38:37 -0500

    build the create action in the Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,16 @@ class UsersController < ApplicationController
         render json: current_user
     end
 
-    # # POST "/users"
-    # def create 
-    #     user = User.create!(user_params)
-    #     session[:user_id] = user.id
-    #     render json: user, status: :created
-    # end
+    # POST "/users"
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
 
-    # private 
-    # def user_params
-    #     params.permit(:username, :password, :password_confirmation)
-    # end
+    private 
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
 
 end
