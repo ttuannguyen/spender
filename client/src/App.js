e730d0273326d5fcd9a62b2487337bce6a087c5f
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-01-17 00:01:37 -0500

    install redux and react-redux and add reducers folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Categories from './components/Categories';
@@ -10,13 +10,28 @@ import Login from './components/Login';
 
 const App = () => {
 
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('/categories')
+    .then(res => res.json()) 
+    .then(data => {
+      setCategories(data)
+    })
+  }, [])
+
+  console.log(categories)
+  
+
+  // console.log(categories)
+
   return (
     <div className="App">
       <Router >
         <h1>Capstone Project</h1>
         <Navbar />
         <Routes>
-          <Route exact path="/categories" element={ <Categories /> } />
+          <Route exact path="/categories" element={ <Categories categories={categories} /> } />
           <Route exact path="/home" element={ <Home /> } />
         </Routes>
       </Router>
--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Categories = () => {
+const Categories = ({categories}) => {
+
+  console.log(categories)
+
+  const categoriesList = categories.map(category => <li>{category.name}</li>)
+
+
   return (
-    <div>Categories</div>
+    <div>
+      {categoriesList}
+    </div>
   )
 }
 
