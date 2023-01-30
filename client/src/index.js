6913ce77c49cc0ea3db5ed4e8bc730fb47c69af5
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-01-30 17:49:54 -0500

    add nested persists using Redux Persist in store

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { persistor ,store } from './redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 
 // import store, wrap App inside Provide and pass store in as prop
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ client/src/redux/store.js
@@ -0,0 +1,44 @@
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/CounterSlice';
+import categoriesReducer from '../features/categories/CategoriesSlice';
+import expensesReducer from '../features/expenses/ExpensesSlice';
+import authReducer from '../features/auth/AuthSlice';
+import userReducer from '../features/user/UserSlice';
+// added after installing redux-persist
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+// import thunk from 'redux-thunk';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducer = combineReducers({ 
+    counter: counterReducer,
+    categories: categoriesReducer,
+    expenses: expensesReducer,
+    auth: authReducer,
+    user: userReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    // middleware: [thunk] // might not need
+})
+
+
+
+// export const store = configureStore({
+//     reducer: {
+//         counter: counterReducer,
+//         categories: categoriesReducer,
+//         expenses: expensesReducer,
+//         auth: authReducer,
+//         user: userReducer
+//     }
+// })
+
+export const persistor = persistStore(store)
