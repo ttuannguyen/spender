18083f003fb9b4e87c983ef8c4c96cbaee8f5212
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-02-10 15:13:31 -0500

    add function to handle record invalid related errors in the Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   before_action :authorized
   include ActionController::Cookies
+  
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
   
   def authorized
     return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
   end
 
-  def current_user
-    User.find_by(id: session[:user_id])
+  private
+  def render_unprocessable_entity(invalid) 
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
 end
