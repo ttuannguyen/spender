aa489b6a30b551a6ec5fdfdb1bfad20011c0d122
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-02-17 13:11:52 -0500

    update config

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_020025) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "expenses", force: :cascade do |t|
-    t.string "merchant"
-    t.string "date"
-    t.float "amount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.integer "category_id", null: false
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # When re-seed, we need to remove the validations first
 
-User.destroy_all
-Category.destroy_all
-Expense.destroy_all
-Note.destroy_all
+# User.destroy_all
+# Category.destroy_all
+# Expense.destroy_all
+# Note.destroy_all
 
 
 
-u1 = User.create(username: "john", password: "asdfghjk")
-u2 = User.create(username: "pikachu", password: "asdfghjk")
+# u1 = User.create(username: "john", password: "asdfghjk")
+# u2 = User.create(username: "pikachu", password: "asdfghjk")
 
-c1 = Category.create(name: "Rent & Utilities")
-c2 = Category.create(name: "Groceries")
-c3 = Category.create(name: "Entertainment")
-c4 = Category.create(name: "Essentials")
-c5 = Category.create(name: "Transportation")
-c6 = Category.create(name: "Subscriptions")
+# c1 = Category.create(name: "Rent & Utilities")
+# c2 = Category.create(name: "Groceries")
+# c3 = Category.create(name: "Entertainment")
+# c4 = Category.create(name: "Essentials")
+# c5 = Category.create(name: "Transportation")
+# c6 = Category.create(name: "Subscriptions")
 
 
-e1 = Expense.create(merchant: "FirstService Residential", date: "1/1/2023", amount: 2000.00, user: u1, category: c1)
-e2 = Expense.create(merchant: "Trader Joe's", date: "1/14/2023", amount: 16.00, user: u1, category: c2)
-e3 = Expense.create(merchant: "Whole Foods", date: "1/14/2023", amount: 25.00, user: u2, category: c2)
+# e1 = Expense.create(merchant: "FirstService Residential", date: "1/1/2023", amount: 2000.00, user: u1, category: c1)
+# e2 = Expense.create(merchant: "Trader Joe's", date: "1/14/2023", amount: 16.00, user: u1, category: c2)
+# e3 = Expense.create(merchant: "Whole Foods", date: "1/14/2023", amount: 25.00, user: u2, category: c2)
 
-n1 = Note.create(content: "Rent is due by the 5th of every month.", user: u1)
+# n1 = Note.create(content: "Rent is due by the 5th of every month.", user: u1)
 
 
 
