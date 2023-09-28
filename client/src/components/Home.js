515172de7bccdfa513d7cdab8a5e5c34006dcbe5
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-09-28 12:34:52 -0700

    update the usage instructions of the app

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,9 +13,8 @@ const Home = () => {
           <h3>Welcome, {user.username}!</h3>
           <h4>Usage Instructions</h4>
           <h5>Keep all of your important expenses in one place with Spender!</h5>
-          <p>Go to Categories to view a list of expense categories. If you need to add a new expense category, use the Category Add Form. </p>
-          <p>To add a new expense, navigate to Add Expense and use the form to add a new expense. Be sure to select the category that the expense belongs to. </p>
-          <p>To view a list of your expenses in that category, navigate to My Expenses by Category.</p>
+          <p>Go to Budgets to view a list of available budgets. Select a budget to add to your budgets list. </p>
+          <p>To create a new expense under a budget, navigate to Add Expense and fill out the add form.</p>
           <p>Use "My Notes" to journal your spending habits.</p>
       </div>
     )
